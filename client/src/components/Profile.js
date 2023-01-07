2734a117bf52dbb9214751bdb5c11172fff0b03e
ShaRinGanIAmino <aminoschabbeh12345@gmail.com>
2023-01-07 23:52:05 +0100

    loader added

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useAuth } from "../hooks/auth";
-import User from "../images/User.svg";
-import jwt from "jwt-decode";
-import api from "../components/api";
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../hooks/auth';
+import User from '../images/User.svg';
+import jwt from 'jwt-decode';
+import api from '../components/api';
 
 const Profile = () => {
   const [user, setUser] = useState(null);
@@ -20,22 +20,22 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="flex gap-4 items-center ml-20">
+    <div className="flex gap-16 items-center ml-20">
       <div className=" rounded-[50%] overflow-hidden">
         <img
           src={
-            "https://media.licdn.com/dms/image/C4E03AQGps13LeSz5KA/profile-displayphoto-shrink_400_400/0/1624627969898?e=1678320000&v=beta&t=kG9JlFxKswUt4ddX-1135n2tU_1TRw7_g7H04Dwn2CY"
+            'https://media.licdn.com/dms/image/C4E03AQGps13LeSz5KA/profile-displayphoto-shrink_400_400/0/1624627969898?e=1678320000&v=beta&t=kG9JlFxKswUt4ddX-1135n2tU_1TRw7_g7H04Dwn2CY'
           }
           alt="user"
           className=" w-40 h-40"
         />
       </div>
-      <div>
+      <div className="flex flex-col gap-4">
         <p className=" text-2xl mb-1">
-          {(user && user.username) || "Chebbi Mohamed Aziz"}
+          {(user && user.username) || 'Chebbi Mohamed Aziz'}
         </p>
         <p className=" text-base">
-          {(user && user.email) || "chebbim106@gmail.com"}
+          {(user && user.email) || 'chebbim106@gmail.com'}
         </p>
       </div>
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        blue: "#24293F",
+        blue: '#24293F',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        old: ['Old English Text MT', 'sans-serif'],
+        mont: ['Montserrat', 'sans-serif'],
       },
     },
   },
