148757ebe4f9e4402da62394a80f034dd164be5f
Ala Baganne <alabaganne9@gmail.com>
2023-01-07 18:54:45 +0100

    Add endpoint to retrieve data of current authenticated user

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,11 +3,20 @@ let { body, validationResult } = require('express-validator');
 let User = require('../models/user');
 
 const userProjections = ['_id', 'name', 'username', 'email'];
+const authMiddleware = require('../middlewares/auth');
 
 router.get('/', async function(req, res) {
 	let users = await User.find({}, userProjections);
 
 	res.send(users);
+});
+
+router.get('/current', authMiddleware, async function(req, res) {
+	let { userId } = req;
+	console.log('userId', userId);
+	let user = await User.findOne({ _id: userId }, userProjections);
+
+	res.send(user);
 });
 
 router.get('/:id/', async function(req, res) {
